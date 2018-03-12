e5d6796082626f4e1c2382f0c09eeebfc67344f2
WillieT1986 <WarMad2@Hotmail.com>
2018-03-11 21:48:48 -0400

    Tried another way after talking with Ben. Tried to go of a sample from Ben and Jasmine states ReferenceError: toggleNavLiLinks is not defined. Seems like I cannot get passed what Jasmine wants defined...

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,14 +1,16 @@
 describe('Testing the functionality, adding js to index', ()=> {
 	//When the Nav Button is click, toggle hide/show for a nav link
 	describe('nav link button clicks', () => {
-		let button;
+	const underTest = toggleNavLiLinks;
 
-		beforeEach(() => {
-			button = new navLinkBtn();
-		});
+	let testCheck = document.createElement('check');
+	testCheck.onClick.toggle(hide);
+	testCheck.onClick.toggle(show);
 
-		it('should toggle hide when button is clicked', () => {
-			expect(button.click());
-		});
+	underTest(check);
+
+	let changedOnClick = testCheck.onClick;
+	expect(changedOnClick).toggle();
+
 	});
 });
