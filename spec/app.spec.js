f5c5ed68083f136ff1588ddc804bb529474bb127
WillieT1986 <WarMad2@Hotmail.com>
2018-03-11 20:27:05 -0400

    Experimenting with TDD a button. Found example online for button click to try and figure out how to use Jasmine.

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,4 +1,24 @@
 describe('Testing the functionality, adding js to index', ()=> {
+	//When the Nav Button is click, toggle hide/show for a nav link
+	describe('toggle hide navLiLink1', () => {
+		it('button clicked', () => {
+			this.button('click');
+			expect(this.click).toggle().hide().show();	
+		});
+	});
+});
 
-	
-})
+	// beforeEach(function () {
+    //     this.clickEventStub = sinon.stub(this, 'clickEvent');
+    // });
+
+    // afterEach(function () {
+    //     this.clickEvent.restore();
+    // });
+
+    // describe('when item is clicked', function () {
+    //     it('event is fired', function () {
+    //         this.elem.trigger('click');
+    //         expect(this.clickEventStub).toHaveBeenCalled();
+    //     });
+    // });
