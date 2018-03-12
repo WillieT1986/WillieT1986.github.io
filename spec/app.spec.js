c2e3f5fefdcb2138561f3a22eab25f1382f80021
WillieT1986 <WarMad2@Hotmail.com>
2018-03-11 20:52:33 -0400

    Deleted example code I found...

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,24 +1,6 @@
 describe('Testing the functionality, adding js to index', ()=> {
 	//When the Nav Button is click, toggle hide/show for a nav link
 	describe('toggle navLiLink1', () => {
-		it('button clicked', () => {
-			this.navLiLink1('click');
-			expect(this.click).hide();
-		});
+		
 	});
 });
-
-	// beforeEach(function () {
-    //     this.clickEventStub = sinon.stub(this, 'clickEvent');
-    // });
-
-    // afterEach(function () {
-    //     this.clickEvent.restore();
-    // });
-
-    // describe('when item is clicked', function () {
-    //     it('event is fired', function () {
-    //         this.elem.trigger('click');
-    //         expect(this.clickEventStub).toHaveBeenCalled();
-    //     });
-    // });
