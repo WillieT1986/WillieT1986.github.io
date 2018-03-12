c1a2c31366e584f429d65c743d8c085eed884fca
WillieT1986 <WarMad2@Hotmail.com>
2018-03-11 21:06:42 -0400

    Changed const to let, added a ',' to it(). Jasmine states thats navLink1 is not defined...

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,14 @@
 describe('Testing the functionality, adding js to index', ()=> {
 	//When the Nav Button is click, toggle hide/show for a nav link
-	describe('toggle navLiLink1', () => {
-		
+	describe('toggle nav links', () => {
+		let navLinks;
+
+		beforeEach(() => {
+			navLinks = new navLink1();
+		});
+
+		it('should toggle hide when button is clicked', () => {
+			expect(navLinks.toggle()).hide();
+		});
 	});
 });
