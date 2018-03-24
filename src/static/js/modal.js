e32d286bcb3d01d167926f729f8a7610af122ba4
WillieT1986 <WarMad2@Hotmail.com>
2018-03-24 01:10:02 -0400

    Added JScript to modal images. Created project-modal.js and linked to git-projects.html

--- src/static/js/modal.js
+++ src/static/js/modal.js
@@ -1,25 +1,20 @@
-// Get the modal
+//Modal for home-page start
 var modal = document.getElementById('about-modal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("about-me-modal");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+//Modal for home-page end
+
